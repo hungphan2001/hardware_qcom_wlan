d38945c04a1a78ec160323463590dca9d86e61de
Ahmed ElArabawy <arabawy@google.com>
2019-11-15 12:43:21 -0800

    Wifi: Modify log specifiers for 64bit hex numbers
    This commit replaces "0x%lx" with PRIx64 macro for better portability of
    handling 64bit values in log messages.
    
    Bug: 144576287
    Test: Builds passes successfully
    Change-Id: I8b8cd56c874f87e477b7a78b2c0e5197d77df483

--- qcwcn/wifi_hal/wifi_hal.cpp
+++ qcwcn/wifi_hal/wifi_hal.cpp
@@ -896,7 +896,7 @@ wifi_error wifi_initialize(wifi_handle *handle)
     }
 
     ALOGV("Initialized Wifi HAL Successfully; vendor cmd = %d Supported"
-            " features : %lx", NL80211_CMD_VENDOR, info->supported_feature_set);
+            " features : 0x%" PRIx64, NL80211_CMD_VENDOR, info->supported_feature_set);
 
     if (wifi_is_nan_ext_cmd_supported(iface_handle))
         info->support_nan_ext_cmd = true;
@@ -1895,10 +1895,10 @@ wifi_error wifi_get_supported_feature_set(wifi_interface_handle iface,
     ret = acquire_supported_features(iface, set);
     if (ret != WIFI_SUCCESS) {
         *set = info->supported_feature_set;
-        ALOGV("Supported feature set acquired at initialization : %lx", *set);
+        ALOGV("Supported feature set acquired at initialization : 0x%" PRIx64, *set);
     } else {
         info->supported_feature_set = *set;
-        ALOGV("Supported feature set acquired : %lx", *set);
+        ALOGV("Supported feature set acquired : 0x%" PRIx64, *set);
     }
     return WIFI_SUCCESS;
 }
